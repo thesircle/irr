cc6694354622e7642fd29d5558be84c5f8e27380
ahmad raza <ahmad.raza@itboost.com>
2017-05-10 10:22:44 +0500

    commited

--- src/components/integrations/integrationInfoComp.tsx
+++ src/components/integrations/integrationInfoComp.tsx
@@ -1,5 +1,10 @@
 export const IntegrationInfoComp = (prop) => {
-    debugger
+
+    var modifyString = (stringValue) => {
+        var bolded = [ "ConnectWise Client" , "System" , "Setup Tables" , '/"Integrator"/' , "Integrator Login" ,
+            '/"+"/' ,"Admin rights" ,"Access Level" , "All Records" , "Save" , "Close" ]
+    }
+
   return(
       <div className="col-lg-6 col-xs-12 img-display">
         <div className="widget-item f-w col-lg-4 hidden-xs">
@@ -18,9 +23,9 @@ export const IntegrationInfoComp = (prop) => {
                   className="badge bg-complete text-white" key={index.toString()}>{obj.key.toString()}</span>&nbsp;
                   {typeof obj.value == "string" ?
                       obj.value :
-                      (<div>{obj.value.head}<ul>
+                      (<span>{obj.value.head}<ul className="subList">
                           {obj.value.detail_List.map((subValue,i) => <li key={i}>{subValue}</li>)}
-                      </ul></div>)}
+                      </ul></span>)}
               </li>
               <div className="line-height">&nbsp;</div>
             </div>)}

22f3e3878ef0cee0cddb9ad3f84cfbb3b2a4527a
Masood <masood.ahmad@itboost.com>
2017-06-08 22:24:44 +0500

    added autoprefixer

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,8 @@
-let babelOptions = {
+const babelOptions = {
     "presets": ["es2015","react", "stage-0"]
 };
+const autoprefixer = require('autoprefixer')
+
 module.exports = {
  entry: './src/index.tsx',
  output: {
@@ -8,42 +10,76 @@ module.exports = {
      path: __dirname
  },
     module: {
-        rules: [{
-            test: /\.ts(x?)$/,
-            exclude: /node_modules/,
-            use: [
-                {
-                    loader: 'babel-loader',
-                    options: babelOptions
-                },
-                {
-                    loader: 'ts-loader'
-                }
-            ]
-        }, {
-            test: /\.js$/,
-            exclude: /node_modules/,
-            use: [
-                {
-                    loader: 'babel-loader',
-                    options: babelOptions
-                }
-            ]
-        },{
+      rules: [{
+          test: /\.ts(x?)$/,
+          exclude: /node_modules/,
+          use: [
+            {
+              loader: 'babel-loader',
+              options: babelOptions
+            },
+            {
+              loader: 'ts-loader'
+            }
+          ]
+      },{
+          test: /\.js$/,
+          exclude: /node_modules/,
+          use: [
+            {
+              loader: 'babel-loader',
+              options: babelOptions
+            }
+          ]
+      },{
           test: /\.json$/,
           exclude: /(node_modules)/,
           loader: "json-loader"
+      },
+        {
+          test: /\.css$/,
+          use:[
+            {
+              loader: 'style-loader'
+            },
+            {
+              loader: 'css-loader'
+            },
+            {
+              loader: 'postcss-loader',
+              options: {
+                plugins: function () {
+                  return [autoprefixer('last 5 versions')]
+                }
+              }
+            }
+          ]
         },
-          {
-            test: /\.css$/,
-            loader: 'style-loader!css-loader!postcss-loader'
-          },
-          {
-            test: /\.scss$/,
-            loader: 'style-loader!css-loader!postcss-loader!sass-loader'
-          }
+        {
+          test: /\.scss$/,
+          use: [
+            {
+              loader: 'style-loader'
+            },
+            {
+              loader: 'css-loader'
+            },
+            {
+              loader: 'postcss-loader',
+              options: {
+                plugins: function () {
+                  return [autoprefixer('last 5 versions')]
+                }
+              }
+            },
+            {
+              loader: 'sass-loader'
+            }
+          ]
+          // loader: 'style-loader!css-loader!postcss-loader!sass-loader'
+        }
 
-        ]
+      ]
     },
  resolve: {
    extensions: [".tsx", ".ts", ".js", "json"]

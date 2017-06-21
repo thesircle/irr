7968e97d2951241662df9de5b3cea00e842f72df
Syed Ahsan Ali <ahsan.ali@itboost.com>
2017-06-20 22:03:52 -0700

    action file changes

--- src/actions/actions.tsx
+++ src/actions/actions.tsx
@@ -3,28 +3,27 @@ import {get,post} from "../utils/nioUtils";
 import {system as S} from "../constants/system";
 import {types as T} from "../constants/types";
 
-
-export const viewDomainTracker:any= (): any => (dispatch:any) : void => {
+export const viewDomainTracker= () => (dispatch:Function) => {
   dispatch({
     type:T.INTEGRATIONS.DOMAINTRACKER.VIEW,
     payload:{domainTrackerFetchingWhois:true}
   });
   setTimeout(() => {
-    get(S.BK.DOMAINTRACKER.GET).then(({body}:any) => {
+    get(S.BK.DOMAINTRACKER.GET).then(({body}) => {
       dispatch({
         type:T.INTEGRATIONS.DOMAINTRACKER.VIEW,
         payload:{domainTrackerFetching:false,data:body}
       });
     });},10000);
 };
-export  const updateKaseya:any = (url, userName, password):any => (dispatch):void => {
+export  const updateKaseya = (url:string, userName:string, password:string) => (dispatch:Function) => {
   //TODO: resolve issue of any with variable invoking method
   let postBody:object = {
     "KaseyaUrl":url,
     "KaseyaUser":userName,
     "KaseyaUserPass":password
   };
-  post(S.BK.API.KASEYA_UPDATE,postBody).then(({body}:any) => {
+  post(S.BK.API.KASEYA_UPDATE,postBody).then(({body}) => {
     dispatch ({
       type: T.INTEGRATIONS.KASEYA.UPDATE,
       payload: {url, userName, password}
@@ -35,7 +34,7 @@ export  const updateKaseya:any = (url, userName, password):any => (dispatch):voi
   // dispatch({type: T.CANCEL_FETCHING}) //TODO
     });
 };
-export const viewKaseya:any = ():any => (dispatch):void => {
+export const viewKaseya = () => (dispatch:Function) => {
   dispatch(addWiseMessage(S.WISE_MESSAGE.ERROR,
                           "newErrorMessage",
                           "Custom Heading"));
@@ -51,7 +50,7 @@ export const viewKaseya:any = ():any => (dispatch):void => {
     type: "fetching",
     payload: {kaseyaFetching: true}
   });
-  get(S.BK.API.KASEYA_VIEW).then(({body}:any) => {
+  get(S.BK.API.KASEYA_VIEW).then(({body}) => {
     let url:string = body.data.KaseyaUrl;
     let userName:string = body.data.KaseyaUser;
     dispatch ({
@@ -65,13 +64,13 @@ export const viewKaseya:any = ():any => (dispatch):void => {
     });
 };
 
-export const addWiseMessage:any = (type, message, heading=""):any => (dispatch):void => {
+export const addWiseMessage = (type:string, message:string, heading="") => (dispatch:Function) => {
   dispatch({
     type: T.WISE_MESSAGE.ADD,
     payload: {type,message,heading} // same as {type:type,message:message,heading:heading} in es6
   });
 };
-export const clearWiseMessage:any = (index):any => (dispatch):void => {
+export const clearWiseMessage = (index:string) => (dispatch:Function) => {
   dispatch({
     type: T.WISE_MESSAGE.CLEAR,
     payload: index
